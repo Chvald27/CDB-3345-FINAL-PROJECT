d309efabf1855d16b3540795e8ab1ea6a09c9c5e
Christian Valencia <christianval27@gmail.com>
2025-08-05 13:58:19 -0400

    Adding artifacts for unit-test.log

--- config.py
+++ config.py
@@ -1,20 +1,19 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ["SECRET_KEY", 'defaultsecret']
+    SECRET_KEY = os.environ.get("SECRET_KEY", "defaultsecret")
 
-    DB_USER = os.environ["DB_USER", "postgres"]
-    DB_PASSWORD = os.environ["DB_PASSWORD", "DevOps2024*"]
-    DB_HOST = os.environ["DB_HOST", "localhost"]
-    DB_NAME = os.environ["DB_NAME", "webappdb"]
+    DB_USER = os.environ.get("DB_USER", "postgres")
+    DB_PASSWORD = os.environ.get("DB_PASSWORD", "DevOps2024*")
+    DB_HOST = os.environ.get("DB_HOST", "localhost")
+    DB_NAME = os.environ.get("DB_NAME", "webappdb")
 
     SQLALCHEMY_DATABASE_URI = (
         f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
     )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_TYPE = "filesystem"
-
-
 
 
 
